6925c5c87f5601e35d36630a3becb06e58aed9de
Oscar TG <oscar.trego@gmail.com>
2023-05-15 17:16:35 -0600

    Primera versión TS

---
+++ dist/index.js
@@ -0,0 +1,37 @@
+"use strict";
+let mensaje = "Hola Mundo";
+mensaje = "Oscar TG";
+mensaje = "bye Oscar";
+console.log(mensaje);
+console.log(typeof []);
+let extincionDinosaurios = 76000000;
+let dinosaurioFavorito = "T-Rex";
+let extintos = true;
+let miVariable;
+miVariable = "Oscar TG";
+miVariable = 42;
+function chanchitoFeliz(config) {
+    return config;
+}
+let animales = ["Chanchito", "Feliz", "Felipe"];
+let nums = [1, 2, 3];
+let checks = [];
+let nums2 = [];
+let tupla = [1, ["Chanchito feliz", "Chanchito feliz"]];
+var Talla;
+(function (Talla) {
+    Talla["Chica"] = "s";
+    Talla["Meidana"] = "m";
+    Talla["Grande"] = "g";
+    Talla["ExtraGrande"] = "xl";
+})(Talla || (Talla = {}));
+const variable1 = Talla.Grande;
+console.log(variable1);
+const estado = 2;
+const objeto = {
+    id: 1,
+    nombre: "Oscar TG",
+    talla: Talla.Meidana,
+};
+console.log(objeto);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,78 @@
+let mensaje: string = "Hola Mundo";
+
+mensaje = "Oscar TG";
+
+mensaje = "bye Oscar";
+
+console.log(mensaje);
+
+console.log(typeof []);
+
+let extincionDinosaurios = 76_000_000;
+let dinosaurioFavorito = "T-Rex";
+let extintos = true;
+
+let miVariable;
+miVariable = "Oscar TG";
+miVariable = 42;
+
+function chanchitoFeliz(config: any) {
+  return config;
+}
+
+let animales: string[] = ["Chanchito", "Feliz", "Felipe"];
+let nums: number[] = [1, 2, 3];
+let checks: boolean[] = [];
+let nums2: Array<number> = [];
+
+// nums.map(x => x.) //El autocompleado sugiere metodos del tipo de dato
+
+//Tupla
+let tupla: [number, string[]] = [1, ["Chanchito feliz", "Chanchito feliz"]];
+
+//ENUM
+enum Talla {
+  Chica = "s",
+  Meidana = "m",
+  Grande = "g",
+  ExtraGrande = "xl",
+}
+
+const variable1 = Talla.Grande;
+
+console.log(variable1);
+
+const enum LoadingState {
+  Idle,
+  Loading,
+  Sucess,
+  Error,
+}
+
+const estado = LoadingState.Sucess;
+
+type Direccion = {
+  numero: number;
+  calle: string;
+  pais: string;
+};
+
+type Persona = {
+  readonly id: number;
+  nombre: string;
+  talla: Talla;
+  direccion: Direccion;
+};
+
+const objeto: Persona = {
+  id: 1,
+  nombre: "Oscar TG",
+  talla: Talla.Meidana,
+  direccion: {
+    numero: 219,
+    calle: "Nogal",
+    pais: "mx",
+  },
+};
+
+const arr: Persona[] = [];
